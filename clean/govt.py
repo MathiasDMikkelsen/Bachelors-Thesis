120062c6fafe73064f9c826d72c1baf742b7192b
ivanalekseevdotdot <ivanxalexeev@gmail.com>
2025-03-17 18:24:31 +0100

    just more work...

--- clean/govt.py
+++ clean/govt.py
@@ -9,8 +9,8 @@ np.set_printoptions(suppress=True, precision=8)
 xi = 10.0
 theta = 1.0
 n = 5
-G = 2.0
-phi = np.array([0.03*5, 0.0825*5, 0.141*5, 0.229*5, 0.511*5])
+G = 1.0
+phi = np.array([0.03*5, 0.0825*5, 0.141*5, 0.229*5, 0.5175*5])
 
 # 2. Objective Function (Social Welfare)
 def swf_obj(x):
@@ -20,7 +20,7 @@ def swf_obj(x):
     l = x[n+1:2*n+1]
     
     # Solve the economic model:
-    utilities, agg_polluting, converged, c, d, ell, w, p_d = solver.solve(tau_w, tau_z, l, G, n=n)
+    utilities, agg_polluting, converged, c, d, ell, w, p_d, _ = solver.solve(tau_w, tau_z, l, G, n=n)
     if not converged:
         return 1e10  # Penalize non-convergence
     
@@ -34,19 +34,19 @@ def ic_constraints(x):
     tau_z = x[n]
     l = x[n+1:2*n+1]
     
-    utilities, agg_polluting, converged, c, d, ell, w, p_d = solver.solve(tau_w, tau_z, l, G, n=n)
+    utilities, agg_polluting, converged, c, d, ell, w, p_d, tax_rev = solver.solve(tau_w, tau_z, l, G, n=n)
     if not converged:
         return -np.ones(n*(n-1)) * 1e6  # Large negative penalty if not converged
 
     # Parameters for utility
     alpha, beta, gamma = 0.7, 0.2, 0.2
-    d0 = 0.1
+    d0 = 0.2
     T = 1.0
-
+    
     # Compute income measure I for each type:
     I = np.zeros(n)
     for j in range(n):
-        I[j] = (T - ell[j])*(1.0 - tau_w[j])*phi[j]*w + l[j]*
+        I[j] = (T - ell[j])*(1.0 - tau_w[j])*phi[j]*w + l[j]*tax_rev
 
     g_list = []
     for i in range(n):
@@ -85,23 +85,23 @@ def gov_constraint(x):
     tau_z = x[n]
     l = x[n+1:2*n+1]
     
-    _, agg_polluting, converged, c, d, ell, w, p_d = solver.solve(tau_w, tau_z, l, G, n=n)
-    return (1 - np.sum(l))*(np.sum(tau_w * phi * w) + tau_z * agg_polluting) - G
+    _, agg_polluting, converged, c, d, ell, w, p_d, tax_rev = solver.solve(tau_w, tau_z, l, G, n=n)
+    return (1 - np.sum(l))*(np.sum(tau_w * phi * w*(1-ell)) + tau_z * agg_polluting) - G
 
 # 5. Solve the Planner Problem
-# Initial guesses: note lump-sum shares sum to 1.
+# Since lump sum shares should not sum to 1, we choose an initial guess that does not sum to 1.
 initial_tau_w = [0.05] * n
 initial_tau_z = [3.0]
-initial_l = [1.0 / n] * n
+initial_l = [0.2] * n   # Sum is 0, not 1
 initial_guess = np.array(initial_tau_w + initial_tau_z + initial_l)
 
 # Define bounds for each variable:
-bounds = [(-5.0, 5.0)] * n + [(0.1, 100.0)] + [(-5.0, 5.0)] * n
+bounds = [(-2.0, 2.0)] * n + [(0.1, 100.0)] + [(-2.0, 2.0)] * n
 
+# For now we only enforce the government spending constraint; you can enable the IC constraints as needed.
 constraints = [
-    #{'type': 'eq', 'fun': lambda x: np.sum(x[n+1:2*n+1]) - 1.0},  # Lump-sum shares constraint
-    {'type': 'eq', 'fun': gov_constraint},                        # Gov spending constraint
-    #{'type': 'ineq', 'fun': ic_constraints}                         # IC constraints
+    {'type': 'eq', 'fun': gov_constraint},
+    {'type': 'ineq', 'fun': ic_constraints}  # Uncomment if you want to enforce IC constraints.
 ]
 
 res = minimize(swf_obj, initial_guess, method='SLSQP', bounds=bounds, constraints=constraints)
@@ -121,3 +121,4 @@ if res.success:
 else:
     print("Optimization Failed!")
     print("Solver message:", res.message)
+    
