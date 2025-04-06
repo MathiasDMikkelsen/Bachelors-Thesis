458a34cee808ef29b5f8edfe122b12c659c20f5f
ivanalekseevdotdot <ivanxalexeev@gmail.com>
2025-04-06 13:05:18 +0200

    figs

--- fix_tauw.py
+++ fix_tauw.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from inner_solver import solve
 
-# Fixed parameters for the model
-tau_w = np.array([0.0, 0.0, 0.0, 0.0, 0.00])
+# a. set policy
+tau_w = np.array([0.015, 0.072, 0.115, 0.156, 0.24])
 g = 5.0
 
-# Define a range for tau_z
+# b. range for tau_z
 tau_z_values = np.linspace(0.1, 3.0, 50)
 
-# Preallocate lists for the quantities we want to plot.
+# c. lists to store results
 t_c_values = []
 t_d_values = []
 z_c_values = []
@@ -17,7 +17,7 @@ z_d_values = []
 w_values   = []
 p_d_values = []
 
-# Evaluate the model for each tau_z and collect the results.
+# d. evaluate model and store results
 for tau_z in tau_z_values:
     _, results, _ = solve(tau_w, tau_z, g)
     t_c_values.append(results["t_c"])
@@ -27,39 +27,39 @@ for tau_z in tau_z_values:
     w_values.append(results["w"])
     p_d_values.append(results["p_d"])
 
-# Create a 2x3 grid figure (2 rows, 3 columns) with individual x-axis labels.
-fig, axs = plt.subplots(2, 3, figsize=(8, 4))  # Not sharing x-axis to ensure x-labels on all plots
+# e. plot
+fig, axs = plt.subplots(2, 3, figsize=(8, 4)) 
 
-# Subplot 1: firm input t_c
+# e1. subplot 1
 axs[0, 0].plot(tau_z_values, t_c_values, color="blue")
 axs[0, 0].set_xlabel(r'$\tau_z$', fontsize=8)
 axs[0, 0].set_ylabel(r'$t_c$', fontsize=8)
 
-# Subplot 2: firm input t_d
+# e2. subplot 2
 axs[0, 1].plot(tau_z_values, t_d_values, color="orange")
 axs[0, 1].set_xlabel(r'$\tau_z$', fontsize=8)
 axs[0, 1].set_ylabel(r'$t_d$', fontsize=8)
 
-# Subplot 3: z_c
+# e3. subplot 3
 axs[0, 2].plot(tau_z_values, z_c_values, color="green")
 axs[0, 2].set_xlabel(r'$\tau_z$', fontsize=8)
 axs[0, 2].set_ylabel(r'$z_c$', fontsize=8)
 
-# Subplot 4: z_d
+# e4. subplot 4
 axs[1, 0].plot(tau_z_values, z_d_values, color="red")
 axs[1, 0].set_xlabel(r'$\tau_z$', fontsize=8)
 axs[1, 0].set_ylabel(r'$z_d$', fontsize=8)
 
-# Subplot 5: price p_d
+# e5. subplot 5
 axs[1, 1].plot(tau_z_values, p_d_values, color="purple")
 axs[1, 1].set_xlabel(r'$\tau_z$', fontsize=8)
 axs[1, 1].set_ylabel(r'$p_d$', fontsize=8)
 
-# Subplot 6: wage w
+# e6. subplot 6
 axs[1, 2].plot(tau_z_values, w_values, color="brown")
 axs[1, 2].set_xlabel(r'$\tau_z$', fontsize=8)
 axs[1, 2].set_ylabel(r'$w$', fontsize=8)
 
 plt.tight_layout()
-plt.savefig("combined_figure.pdf")
+plt.savefig("a_impulse_resp.pdf")
 plt.show()
